072f9fc2f3d18d28951ae9c3d9f5373ba5c3e73b
elmambou <77119578+elmambou@users.noreply.github.com>
2021-11-13 14:34:28 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
-// set current year in footer
-//const currentDate = new Date();
-//document.querySelector('#year').textContent = currentDate.getFullYear();
-document.getElementById('year').innerHTML = (new Date).getFullYear();
-const d = new Date();
-d.setDate(d.getDate() + 0);
-document.getElementById("update").innerHTML = "Last Updated: " + " " + d;
+
+var dateOptions = {
+   weekday: "long",
+   day: "numeric",
+   month: "long",
+   year: "numeric",
+}
+document.getElementById('fullDate').innerHTML = new Date().toLocaleString("en-ZA", dateOptions);
